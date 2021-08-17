2d7c96e490810ba676563ed0494788808398d40a
Kirill Ilichev <just.another.kirill.ilichev@gmail.com>
2021-08-17 04:06:34 +0300

    Updated folder list page UI

--- lib/page/folder_list_page/folder_list_page.dart
+++ lib/page/folder_list_page/folder_list_page.dart
@@ -3,9 +3,11 @@ import 'package:provider/provider.dart';
 import 'package:sample_todo_app/config/app_router.dart';
 import 'package:sample_todo_app/model/folder.dart';
 import 'package:sample_todo_app/page/folder_list_page/folder_tile.dart';
+import 'package:sample_todo_app/widget/custom_fab.dart';
 import 'package:sample_todo_app/state/folders_change_notifier.dart';
 import 'package:sample_todo_app/state/todo_change_notifier.dart';
 import 'package:sample_todo_app/widget/custom_scaffold.dart';
+import 'package:sliver_tools/sliver_tools.dart';
 
 class FolderListPage extends StatelessWidget {
   const FolderListPage({Key? key}) : super(key: key);
@@ -15,17 +17,20 @@ class FolderListPage extends StatelessWidget {
     return Consumer<FoldersChangeNotifier>(
       builder: (ctx, folders, ___) => CustomScaffold(
         title: Text('Списки'),
-        body: SliverList(
-          delegate: SliverChildBuilderDelegate(
-            (ctx, idx) {
-              if (idx == 0) return _buildAll(context);
-              if (idx == 1) return _buildToday(context);
-              if (idx == folders.items.length + 2)
-                return _buildAddButton(context);
-              return _buildFolder(context, folders.items[idx - 2]);
-            },
-            childCount: folders.items.length + 3,
+        body: MultiSliver(children: [
+          SliverToBoxAdapter(child: _buildAll(context)),
+          SliverToBoxAdapter(child: _buildToday(context)),
+          SliverToBoxAdapter(child: Divider()),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (ctx, idx) => _buildFolder(ctx, folders.items[idx]),
+              childCount: folders.items.length,
+            ),
           ),
+        ]),
+        floatingActionButton: CustomFab(
+          icon: Icon(Icons.add),
+          onPressed: () => Navigator.pushNamed(context, AppRoute.add_folder),
         ),
       ),
     );
@@ -58,22 +63,6 @@ class FolderListPage extends StatelessWidget {
       description: Text(folder.description),
       filter: TodoFilter(folderId: folder.id),
       count: 1,
-    );
-  }
-
-  Widget _buildAddButton(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      child: TextButton.icon(
-        style: TextButton.styleFrom(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-        ),
-        onPressed: () => Navigator.pushNamed(context, AppRoute.add_folder),
-        icon: Icon(Icons.add),
-        label: Text('Добавить папку'),
-      ),
     );
   }
 }
--- lib/page/todo_list_page/todo_list_page.dart
+++ lib/page/todo_list_page/todo_list_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sample_todo_app/config/app_router.dart';
-import 'package:sample_todo_app/page/todo_list_page/custom_fab.dart';
+import 'package:sample_todo_app/widget/custom_fab.dart';
 import 'package:sample_todo_app/page/todo_list_page/todo_tile.dart';
 import 'package:sample_todo_app/state/todo_change_notifier.dart';
 import 'package:sample_todo_app/widget/custom_scaffold.dart';
