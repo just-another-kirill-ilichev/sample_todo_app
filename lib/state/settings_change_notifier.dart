4003e53623d675fc0b1f5cae60449d9e3e7be396
Kirill Ilichev <just.another.kirill.ilichev@gmail.com>
2021-08-26 04:56:30 +0300

    Added settings saving using shared preferences

--- lib/state/settings_change_notifier.dart
+++ lib/state/settings_change_notifier.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsChangeNotifier extends ChangeNotifier {
-  bool _autosave = false;
+  final SharedPreferences preferences;
+
+  late bool _autosave;
+  late String _locale;
 
   set autosave(bool value) {
     _autosave = value;
+    preferences.setBool('autosave', _autosave);
     notifyListeners();
   }
 
   bool get autosave => _autosave;
+
+  String get locale => _locale;
+
+  SettingsChangeNotifier(this.preferences) {
+    _autosave = preferences.getBool('autosave') ?? false;
+    _locale = preferences.getString('locale') ?? 'ru_RU';
+  }
 }
